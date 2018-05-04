e9833258de5463474b18b4b6c0281d0e3a7a2731
Nishanth <nishanthprakash20@gmail.com>
2018-05-04 12:45:33 -0400

    begin basic synthesizing macros

---
+++ src/synthesizer/synthesizer.lisp
@@ -0,0 +1,33 @@
+
+(defpackage synthesizer
+  (:documentation "Package containing a simple implementation of trees in which nodes and edges can be labelled.
+
+")
+
+  (:use 
+   common-lisp)
+  (:export
+   synthesize))
+
+(in-package synthesizer)
+
+
+(defmacro synthesize (mactions)
+   (let* 
+      ((num-levels (list-length mactions)))
+      `(progn ;,@(gen-levels 2)   
+      ,@(loop for i from 0 to (- num-levels 1) collecting 
+         `(gen-macro ,i 'mactions)))))
+
+
+(defmacro gen-macro (n mactions)
+   (let*
+      ((nth n mactions)
+      ;(package (symbol-package synthesize))
+      (level-fn (intern (format nil "LEVEL-~a" n) *PACKAGE*)))
+      `(defun ,level-fn () 'levelfn-called)))
+
+
+(macroexpand-1 '(gen-macro 1 (1 2 3)))
+
+(macroexpand-1 '(synthesize (1 2 3)))
